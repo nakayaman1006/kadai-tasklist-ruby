46194327e0a673cbd29dd56fd17d555621b6c02d
yuki.nakayama <nakayaman1006@yahoo.co.jp>
2019-11-28 03:12:25 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(content: 'タスク1', status: '対応中')
+Task.create(content: 'タスク2', status: '完了')
+Task.create(content: 'タスク3', status: '未実施')
+Task.create(content: 'タスク4', status: '対応中')
+Task.create(content: 'タスク5', status: '未実施')
+Task.create(content: 'タスク6', status: '未実施')
+Task.create(content: 'タスク7', status: '未実施')
+Task.create(content: 'タスク8', status: '未実施')
+Task.create(content: 'タスク9', status: '未実施')
+Task.create(content: 'task1', status: '未実施')
+Task.create(content: 'task2', status: '未実施')
+Task.create(content: 'task3', status: '未実施')
+Task.create(content: 'task4', status: '未実施')
